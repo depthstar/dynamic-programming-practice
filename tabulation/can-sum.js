79642f8414ff885870ca0400e06a669a4b536955
depthstar <atempovivo@gmail.com>
2021-01-18 16:13:45 -0600

    Initial setup and test cases

---
+++ tabulation/can-sum.js
@@ -0,0 +1,12 @@
+
+const canSum = () => {
+
+}
+
+
+//testing
+console.log(canSum(7, [2, 3])); //true
+console.log(canSum(7, [5, 3, 4, 7])); //true
+console.log(canSum(7, [2, 4])); //false
+console.log(canSum(8, [2, 3, 5])); //true
+console.log(canSum(300, [7, 14])); //false
