e8b5d6e91b877ed1bcc6372310c3d61041b24c91
depthstar <atempovivo@gmail.com>
2021-01-17 23:50:38 -0600

    update to actual examples he uses lol"

--- all-construct.js
+++ all-construct.js
@@ -1,14 +1,26 @@
 
 const allConstruct = (target, wordBank) = {
-    
+    if (target === '') return [[]];
+    if (target ) return [];
+
 
 }
 
 
 
-console.log(countConstruct("purple", ['purp', 'p', 'ur', 'le', 'purpl'])); //2
-console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); //1
-console.log(countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])); //0
+console.log(countConstruct("purple", ['purp', 'p', 'ur', 'le', 'purpl']));
+//[
+    //  ['purp', 'le'],
+    //  ['p', 'ur', 'p', 'le']
+//]
+console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]));
+//[
+    //['ab', 'cd', 'ef'],
+    //['ab', 'c', 'def'],
+    //['abc', 'def'],
+    //['abcd', 'ef']
+//]
+console.log(countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])); //[]
 console.log(countConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])); //4
 
 console.log(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
@@ -21,4 +33,4 @@ console.log(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
     "eeeeee",
     "eeeeeee"
 ]
-)); // 0
+)); // []]
