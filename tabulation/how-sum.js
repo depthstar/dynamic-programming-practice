c82b93f5f9699f1a181856ff25682d1a6fc95b47
depthstar <atempovivo@gmail.com>
2021-01-18 16:30:09 -0600

    Initial setup and test cases

---
+++ tabulation/how-sum.js
@@ -0,0 +1,11 @@
+
+const howSum = (targetSum, numbers) => {
+
+}
+
+
+//testing
+console.log(howSum(7, [2, 3])); // [3, 2, 2]
+console.log(howSum(7, [5, 3, 4, 7])); // [4, 3]
+console.log(howSum(8, [5, 3, 2])); // [2, 2, 2, 2]
+console.log(howSum(300, [7, 14])); // null
