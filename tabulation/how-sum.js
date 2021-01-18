cca582d22a1b383c484a04cf63a59ff9df8a0b51
depthstar <atempovivo@gmail.com>
2021-01-18 16:40:33 -0600

    tabulation solution

--- tabulation/how-sum.js
+++ tabulation/how-sum.js
@@ -1,8 +1,22 @@
 
 const howSum = (targetSum, numbers) => {
+    const table = Array(targetSum + 1).fill(null);
+    table[0] = [];
 
-}
+    for (let i = 0; i <= targetSum; i++) {
+        if (table[i] !== null) {
+            for (let num of numbers) {
+                table[i + num] = [ ...table[i], num ];
+            }
+        }
+    }
+    return table[targetSum];
+};
 
+
+//complexity
+//time: O(m^2*n)
+//space: O(m^2)
 
 //testing
 console.log(howSum(7, [2, 3])); // [3, 2, 2]
