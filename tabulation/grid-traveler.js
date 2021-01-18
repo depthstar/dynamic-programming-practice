193a25b4e7b03aecd6059772c39e7d9ec83bae0b
depthstar <atempovivo@gmail.com>
2021-01-18 16:09:08 -0600

    tabulation solution

--- tabulation/grid-traveler.js
+++ tabulation/grid-traveler.js
@@ -1,9 +1,28 @@
 
-const gridTraveler = () => {
+const gridTraveler = (m, n) => {
+    const table = Array(m + 1)
+        .fill()
+        .map(() => Array(n + 1).fill(0));
+
+    table[1][1] = 1;
+
+    for (let i = 0; i<= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            const current = table[i][j];
+            if (j + 1 <= n) table[i][j + 1] += current;
+            if (i + 1 <= m) table[i + 1][j] += current;
+        }
+    }
+    return table[m][n];
+
+
 
 }
 
 
+//complexity tabulated
+//O(mn) time
+//O(mn) space
 console.log(gridTraveler(1, 1)); //1
 console.log(gridTraveler(2, 3)); //3
 console.log(gridTraveler(3, 2)); //3
