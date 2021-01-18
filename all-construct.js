644ac3c68cd6d3d825d05eb53bdf4770d8953122
depthstar <atempovivo@gmail.com>
2021-01-17 23:58:57 -0600

    brute force solution

--- all-construct.js
+++ all-construct.js
@@ -1,29 +1,38 @@
 
-const allConstruct = (target, wordBank) = {
+const allConstruct = (target, wordBank) => {
     if (target === '') return [[]];
-    if (target ) return [];
 
-
+    const result = [];
+    
+    for (let word of wordBank) {
+        if (target.indexOf(word) === 0) {
+            const suffix = target.slice(word.length);
+            const suffixWays = allConstruct(suffix, wordBank);
+            const targetWays = suffixWays.map(way => [ word, ...way]);
+            result.push(...targetWays);
+        }
+    }
+    return result
 }
 
 
 
-console.log(countConstruct("purple", ['purp', 'p', 'ur', 'le', 'purpl']));
+console.log(allConstruct("purple", ['purp', 'p', 'ur', 'le', 'purpl']));
 //[
     //  ['purp', 'le'],
     //  ['p', 'ur', 'p', 'le']
 //]
-console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]));
+console.log(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]));
 //[
     //['ab', 'cd', 'ef'],
     //['ab', 'c', 'def'],
     //['abc', 'def'],
     //['abcd', 'ef']
 //]
-console.log(countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])); //[]
-console.log(countConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])); //4
+console.log(allConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])); //[]
+console.log(allConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])); //4
 
-console.log(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+console.log(allConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
     "f",
     "e",
     "ee",
