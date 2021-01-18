31c89a7f052dc5f6b27e841a8d4aaf77ba80e75d
depthstar <atempovivo@gmail.com>
2021-01-18 15:53:35 -0600

    First setup with test cases

---
+++ tabulation/grid-traveler.js
@@ -0,0 +1,11 @@
+
+const gridTraveler = () => {
+
+}
+
+
+console.log(gridTraveler(1, 1)); //1
+console.log(gridTraveler(2, 3)); //3
+console.log(gridTraveler(3, 2)); //3
+console.log(gridTraveler(3, 3)); //6
+console.log(gridTraveler(18, 18)); //2333606220
