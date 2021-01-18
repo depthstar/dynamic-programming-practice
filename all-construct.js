5fb7a6b7c30814587d451c941ee2227f1fcb58f9
depthstar <atempovivo@gmail.com>
2021-01-17 23:39:57 -0600

    initial structure and test cases

---
+++ all-construct.js
@@ -0,0 +1,24 @@
+
+const allConstruct = (target, wordBank) = {
+    
+
+}
+
+
+
+console.log(countConstruct("purple", ['purp', 'p', 'ur', 'le', 'purpl'])); //2
+console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); //1
+console.log(countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])); //0
+console.log(countConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])); //4
+
+console.log(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+    "f",
+    "e",
+    "ee",
+    "eee",
+    "eeee",
+    "eeeee",
+    "eeeeee",
+    "eeeeeee"
+]
+)); // 0
