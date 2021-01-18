057ffe2119211c780291b433080347946872a24a
depthstar <atempovivo@gmail.com>
2021-01-18 16:42:27 -0600

    bestsum setup and test cases

---
+++ tabulation/best-sum.js
@@ -0,0 +1,10 @@
+
+const bestSum = () => {
+
+}
+
+//testing
+console.log(bestSum(7, [5, 3, 4, 7])); // [7]
+console.log(bestSum(8, [2, 3, 5])); // [3, 5]
+console.log(bestSum(8, [1, 4, 5])); // [4, 4]
+console.log(bestSum(100, [1, 2, 5, 25])); // [25, 25, 25, 25]
